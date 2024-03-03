a96bfeabd61b7088f2858783160e854cd02a027b
tanishkalbus <138288874+tanishkalbus@users.noreply.github.com>
2024-03-03 10:49:51 +0000

    conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,70 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let Valueinnumber = Number(score)
+
+// console.log(typeof Valueinnumber);
+// console.log(Valueinnumber);
+
+// what if the score is inn the form of some variables with number like 33aask
+// let score = "33ac"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let Valueinnumber = Number(score)
+
+// console.log(typeof Valueinnumber);
+// console.log(Valueinnumber);
+
+// then the result of this would be the NaN(not a number )
+
+
+// now in the case of score is equal to null then the result will be zero and if the score is undefined 
+// the result would be NaN and if the score is boolean then for true it is 1 and for false it is 0 
+
+
+// "33" = 33
+// "33ab" = NaN 
+// "true" = 1 , "false"= 0 
+// null = 0 , undefiend = NaN 
+//  THIS IS OF THE NUMBER CONVERSION 
+
+
+
+
+//  BOOLENA CONVERSION 
+
+/*let anumber = undefined
+
+let changeintoboolean = Boolean(anumber)
+console.log(typeof changeintoboolean);
+console.log(changeintoboolean);*/
+
+
+// "1"= true 
+// ""  = false 
+// "tanishk" = true 
+// null = false 
+// undeifned = false 
+
+
+
+// STRING CONVERSION 
+
+let somenumber = undefined
+
+let stringconversion = String(somenumber)
+console.log(stringconversion);
+console.log(typeof stringconversion);
+
+// "33" = 33
+// null,undefined, true = same as (i think it treat them as words)
+
+
+
+
+
+
