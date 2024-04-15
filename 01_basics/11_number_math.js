ee852896a959405c327ba3b24d22848ac12b4f8e
tanishkalbus <138288874+tanishkalbus@users.noreply.github.com>
2024-04-15 09:31:56 +0000

    number and maths

---
+++ 01_basics/11_number_math.js
@@ -0,0 +1,13 @@
+let score = 299
+
+console.log(score);
+
+let balance = new Number(100);
+
+// now due to this new Number it will the number 100 % and it has some property 
+
+// like we can convert it into string and also assume that there is big calculation in making something 
+// so we can use tofixed() to fix a number after decimal 
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
